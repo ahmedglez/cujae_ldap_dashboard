c9a12ce89fb7e56f2daca82ca038f2b305e5ea89
Ahmedglez <ahmediglez@gmail.com>
2023-08-02 19:42:41 -0400

    persist profile store

--- src/stores/profile.store.js
+++ src/stores/profile.store.js
@@ -1,14 +1,32 @@
-import {create} from "zustand";
+import { create } from 'zustand'
 
 const initialState = {
-	user: null,
-};
+  user: null
+}
 
-const useProfileStore = create((set) => ({
-	...initialState,
+const useProfileStore = create(set => ({
+  ...initialState,
 
-	login: (user) => set({ user }),
-	logout: () => set({ user: null }),
-}));
+  login: user => set({ user }),
+  logout: () => set({ user: null })
+}))
 
-export default useProfileStore;
+if (typeof window !== 'undefined') {
+  // Load the state from local storage (if available)
+  const persistedState = JSON.parse(localStorage.getItem('profileStoreState'))
+  useProfileStore.setState(state => ({ ...state, ...persistedState }))
+
+  // Subscribe to state changes and save the state to local storage
+  useProfileStore.subscribe(
+    state => {
+      // Save only the parts of the state you want to persist
+      const persistedState = {
+        user: state.user
+      }
+      localStorage.setItem('profileStoreState', JSON.stringify(persistedState))
+    },
+    state => state // Select which parts of the state to subscribe to
+  )
+}
+
+export default useProfileStore
