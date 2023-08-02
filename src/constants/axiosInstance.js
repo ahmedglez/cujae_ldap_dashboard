0892073e43ac5507af4f90076ca6ffdb6b43fbae
Ahmedglez <ahmediglez@gmail.com>
2023-08-02 17:54:00 -0400

    login working

---
+++ src/constants/axiosInstance.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+require('dotenv').config()
+
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+
+console.log('baseURL', baseURL)
+
+export const withoutAuthAxiosInstance = axios.create({
+  baseURL: baseURL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export const withAuthAxiosInstance = axios.create({
+  baseURL: baseURL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
