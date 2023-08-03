a44d04179e8f9830b2d6f4f09f0f61f9dbbfc61f
Ahmedglez <ahmediglez@gmail.com>
2023-08-03 19:47:18 -0400

    fix correct avatar url

--- src/hooks/useAvatarSelector.js
+++ src/hooks/useAvatarSelector.js
@@ -3,26 +3,27 @@ import useProfileStore from '@/stores/profile.store'
 
 const avatars = {
   M: {
-    Blanco: '/public/images/avatars/3.png',
-    Negro: '/public/images/avatars/7.png'
+    Blanco: '/images/avatars/3.png',
+    Negro: '/images/avatars/7.png'
     // Add more male avatar URLs for different skin colors if needed
   },
   F: {
-    Blanco: '/public/images/avatars/8.png',
-    Negro: '/public/images/avatars/4.png'
+    Blanco: '/images/avatars/8.png',
+    Negro: '/images/avatars/4.png'
     // Add more female avatar URLs for different skin colors if needed
   }
 }
 
 const useAvatarSelector = () => {
   const [selectedAvatar, setSelectedAvatar] = useState(null)
-  const user = useProfileStore(state => state.user)
+  const { user, setAvatar } = useProfileStore(state => state)
   const { skinColor, sex } = user
 
   useEffect(() => {
     // Select the appropriate avatar based on skinColor and sex
-    const avatarUrl = avatars[sex][skinColor] || '/public/images/avatars/1.png'
+    const avatarUrl = avatars[sex][skinColor] || '/images/avatars/1.png'
     setSelectedAvatar(avatarUrl)
+    setAvatar(avatarUrl)
   }, [skinColor, sex])
 
   return selectedAvatar
