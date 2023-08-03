f918e8aa025fc64fe5ecdb80d648fd98025bec57
Ahmedglez <ahmediglez@gmail.com>
2023-08-03 17:03:09 -0400

    hide save button

--- src/views/account-settings/TabInfo.tsx
+++ src/views/account-settings/TabInfo.tsx
@@ -134,14 +134,14 @@ const TabInfo = () => {
               disabled={true}
             />
           </Grid>
-          <Grid item xs={12}>
+          {/* <Grid item xs={12}>
             <Button variant='contained' sx={{ marginRight: 3.5 }}>
               Save Changes
             </Button>
             <Button type='reset' variant='outlined' color='secondary' onClick={() => setDate(null)}>
               Reset
             </Button>
-          </Grid>
+          </Grid> */}
         </Grid>
       </form>
     </CardContent>
