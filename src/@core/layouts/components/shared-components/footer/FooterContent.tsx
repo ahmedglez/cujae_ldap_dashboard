3e46fb07ede59cf6cce26f0a435e90372147e1f9
Ahmedglez <ahmediglez@gmail.com>
2023-08-22 13:44:11 -0400

    Update footer links

--- src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -17,31 +17,19 @@ const FooterContent = () => {
           ❤️
         </Box>
         {` by `}
-        <Link target='_blank' href='https://themeselection.com/'>
-          ThemeSelection
+        <Link target='_blank' href='https://github.com/ahmedglez'>
+          Ahmed Glez
         </Link>
       </Typography>
       {hidden ? null : (
         <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/LICENSE'
-          >
+          <Link target='_blank' href='https://github.com/ahmedglez/cujae_ldap_manager/blob/master/LICENSE'>
             MIT License
           </Link>
-          <Link target='_blank' href='https://themeselection.com/'>
-            More Themes
-          </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/README.md'
-          >
+          <Link target='_blank' href='https://github.com/ahmedglez/cujae_ldap_manager'>
             Documentation
           </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/issues'
-          >
+          <Link target='_blank' href='https://github.com/ahmedglez/cujae_ldap_manager/issues'>
             Support
           </Link>
         </Box>
