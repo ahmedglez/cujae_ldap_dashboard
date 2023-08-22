b73d68ae0783b76bdd60605b28941162605ea1f9
Ahmedglez <ahmediglez@gmail.com>
2023-08-22 13:23:56 -0400

    hidding navbar content

--- src/layouts/components/vertical/AppBarContent.tsx
+++ src/layouts/components/vertical/AppBarContent.tsx
@@ -57,7 +57,7 @@ const AppBarContent = (props: Props) => {
         />
       </Box>
       <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
-        {hiddenSm ? null : (
+        {/*  {hiddenSm ? null : (
           <Box
             component='a'
             target='_blank'
@@ -71,9 +71,9 @@ const AppBarContent = (props: Props) => {
               src='https://img.shields.io/github/stars/themeselection/materio-mui-react-nextjs-admin-template-free?style=social'
             />
           </Box>
-        )}
+        )} */}
         <ModeToggler settings={settings} saveSettings={saveSettings} />
-        <NotificationDropdown />
+        {/* <NotificationDropdown /> */}
         <UserDropdown />
       </Box>
     </Box>
