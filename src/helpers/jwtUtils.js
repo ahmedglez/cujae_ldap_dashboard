0f06d7ef1b4ce166d2e9523ca4aaee33b70b474f
Ahmedglez <ahmediglez@gmail.com>
2023-08-30 12:16:48 -0400

    delete unnecesary utils in jwtUtils

--- src/helpers/jwtUtils.js
+++ src/helpers/jwtUtils.js
@@ -1,14 +1,13 @@
 import jwt_decode from 'jwt-decode'
-import { getItem, saveItem } from './localStorageUtils'
 
 const TOKEN_KEY = 'jwtToken'
 
 export const saveToken = token => {
-  saveItem(TOKEN_KEY, token)
+  localStorage.saveItem(TOKEN_KEY, token)
 }
 
 export const getToken = () => {
-  getItem(TOKEN_KEY)
+  return localStorage.getItem(TOKEN_KEY)
 }
 
 export const decodeJWT = () => {
@@ -27,6 +26,5 @@ export const checkRoles = roles => {
 export const getLastTimeLogged = roles => {
   const jwtToken = localStorage.getItem(TOKEN_KEY)
   const decodedToken = jwt_decode(jwtToken)
-  console.log('decodedToken', decodedToken)
   return decodedToken.last_time_logged
 }
