a5b895aa7ab8faebfd37ffe9d0925cd21b2e5a95
Ahmedglez <ahmediglez@gmail.com>
2023-08-30 12:17:12 -0400

    add jwt interceptor in axios instance

--- src/constants/axiosInstance.js
+++ src/constants/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 require('dotenv').config()
+import { getToken } from '@/helpers/jwtUtils'
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-
 
 export const withoutAuthAxiosInstance = axios.create({
   baseURL: baseURL,
@@ -19,3 +19,17 @@ export const withAuthAxiosInstance = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+withAuthAxiosInstance.interceptors.request.use(
+  config => {
+    const jwtToken = getToken()
+    console.log(`Bearer ${jwtToken}`)
+    if (jwtToken) {
+      config.headers['Authorization'] = `Bearer ${jwtToken}`
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
