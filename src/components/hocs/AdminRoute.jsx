d7aa1118f84d3d01f0aee16ce7924adeb42cdbc9
Ahmedglez <ahmediglez@gmail.com>
2023-08-30 12:17:48 -0400

    fix rendering problem in admin route client side

--- src/components/hocs/AdminRoute.jsx
+++ src/components/hocs/AdminRoute.jsx
@@ -5,14 +5,18 @@ import useProfileStore from '@/stores/profile.store'
 const AdminRoute = WrappedComponent => {
   const WithAdminAccess = props => {
     const router = useRouter()
-    const { isAdmin } = useProfileStore.getState()
 
-    // Check if the user is an admin
-    if (!isAdmin) {
-      router.replace('/')
-      return null
-    }
+    // Wrap the isAdmin check in a useEffect to run on the client side
+    React.useEffect(() => {
+      const { isAdmin } = useProfileStore.getState()
 
+      // Check if the user is an admin
+      if (!isAdmin) {
+        router.replace('/')
+      }
+    }, [])
+
+    // Render the WrappedComponent regardless of the isAdmin check
     return <WrappedComponent {...props} />
   }
 
