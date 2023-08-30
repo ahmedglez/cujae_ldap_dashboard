2af30013c5f91ef6675fd6bf68cee988da8869ab
Ahmedglez <ahmediglez@gmail.com>
2023-08-30 12:51:10 -0400

    add types to admin users page

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,43 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import AdminRoute from '@/components/hocs/AdminRoute'
 import useProfileStore from '@/stores/profile.store'
-import useUserStore from '@/stores/users.store'
+import useUserStore from '@/stores/users.store' // Import the UserStore type
 import { withAuthAxiosInstance } from '@/constants/axiosInstance'
 
 interface User {
   // Define your user object's properties here
 }
 
+interface ResponseData {
+  // Define the structure of your response data
+  data: User[] // Replace 'User' with your actual user object structure
+  // Add other properties if needed
+}
+
 const UsersPage: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<any>(null) // Use a more specific type if possible
   const [page, setPage] = useState<number>(0)
   const [limit, setLimit] = useState<number>(10)
-  const profileStore = useProfileStore(state => state.baseDN)
+  const baseDN = useProfileStore(state => state.baseDN)
   const usersStore = useUserStore()
-
-  console.log('profile', profileStore)
-  console.log('usersStore', usersStore)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log('profileStore.baseDN', profileStore)
         setLoading(true)
-        const response = await withAuthAxiosInstance.post(`/users/baseDN?page=${page}&limit=${limit}`, {
-          baseDN: profileStore
+        const response = await withAuthAxiosInstance.post<ResponseData>(`/users/baseDN?page=${page}&limit=${limit}`, {
+          baseDN: baseDN
         })
         // Handle the response, e.g., set the users in the usersStore
         usersStore.setUsers(response.data.data) // Assuming you have a users state in usersStore
         usersStore.setPagination({ page, limit })
         setLoading(false)
       } catch (err) {
-        console.error('Error', err)
         setLoading(false)
+        setError(err)
       }
     }
 
     fetchData()
-  }, [page, limit, profileStore, usersStore])
+  }, [page, limit, baseDN, usersStore])
 
   return (
     <div>
