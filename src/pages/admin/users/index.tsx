8e993e5da95f4c0487a5d18f27433fd13708659c
Ahmedglez <ahmediglez@gmail.com>
2023-08-31 11:40:15 -0400

    refactor users page states

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,5 +1,5 @@
 import AdminRoute from '@/components/hocs/AdminRoute'
-import { user_types_query } from '@/constants/userTypes'
+import { user_types_query, userTypes } from '@/constants/userTypes'
 import useFetchUsers from '@/hooks/useFetchUsers'
 import useProfileStore from '@/stores/profile.store'
 import EmployeeType from '@/types/employee.type'
@@ -8,6 +8,7 @@ import EmployeesTable from '@/views/admin/users/EmployeesTable'
 import StudentsTable from '@/views/admin/users/StudentsTable'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
+import UserType from '@/types/user.type'
 
 type UsersPageProps = {
   userType: string
@@ -16,7 +17,7 @@ type UsersPageProps = {
 const UsersPage: React.FC<UsersPageProps> = ({ userType }) => {
   const router = useRouter()
   const [page, setPage] = useState<number>(0)
-  const [limit, setLimit] = useState<number>(20)
+  const [limit, setLimit] = useState<number>(50)
   const baseDN = useProfileStore(state => state.baseDN)
 
   const {
@@ -24,23 +25,22 @@ const UsersPage: React.FC<UsersPageProps> = ({ userType }) => {
     error,
     users: responseUsers
   } = useFetchUsers(`/users/baseDN?userType=${userType}&page=${page}&limit=${limit}`, baseDN)
-  const [students, setStudents] = useState<StudentType[]>([])
-  const [employees, setEmployees] = useState<EmployeeType[]>([])
+
+  const [users, setUsers] = useState<UserType[]>([])
+  const students = users?.filter(user => user.userType === userTypes[0]) as StudentType[]
+  const employees = users?.filter(user => user.userType === userTypes[1]) as EmployeeType[]
 
   useEffect(() => {
     if (!loading && responseUsers.length > 0) {
-      if (userType === user_types_query[0]) {
-        setStudents(responseUsers as StudentType[])
-      } else if (userType === user_types_query[1]) {
-        setEmployees(responseUsers as EmployeeType[])
-      }
+      const typedUsers = responseUsers as UserType[]
+      setUsers(typedUsers)
     }
   }, [responseUsers, userType, loading])
 
   return (
     <div>
       <h1>Listado de usuarios</h1>
-      {!loading && responseUsers.length > 0 && (
+      {!loading && users && (
         <>
           {userType === user_types_query[0] && <StudentsTable students={students} />}
           {userType === user_types_query[1] && <EmployeesTable employees={employees} />}
