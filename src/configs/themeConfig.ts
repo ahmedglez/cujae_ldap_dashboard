31a5f8520f92cc3b7d7ae628dbc42ba0773c3e5d
Ahmedglez <ahmediglez@gmail.com>
2023-11-02 23:16:59 -0400

    update application name

--- src/configs/themeConfig.ts
+++ src/configs/themeConfig.ts
@@ -17,7 +17,7 @@ type ThemeConfig = {
 
 const themeConfig: ThemeConfig = {
   // ** Layout Configs
-  templateName: 'CUJAE LDAP Dashboard' /* App Name */,
+  templateName: 'CUJAE LDAP Manager' /* App Name */,
   mode: 'light' /* light | dark */,
   contentWidth: 'boxed' /* full | boxed */,
 
