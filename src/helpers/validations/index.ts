33355b301068945242844bb639d7eb948f034665
Ahmedglez <ahmediglez@gmail.com>
2023-12-08 15:34:43 -0500

    update email regex

--- src/helpers/validations/index.ts
+++ src/helpers/validations/index.ts
@@ -1,4 +1,4 @@
 export const validateEmail = (email: string) => {
-  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/
   return emailPattern.test(email)
 }
